765a39e325920e1a33763dc9956a9360525dc013
joao <joaovitorlimadearaujo@gmail.com>
2026-06-23 18:31:59 -0300

    movie window funcional

--- views/MovieWindow.py
+++ views/MovieWindow.py
@@ -12,22 +12,24 @@ class MovieWindow(ctk.CTkToplevel):
     def __init__(self, parent, movie):
         super().__init__(parent)
 
+        self.state("zoomed")
+
         posterUrl = f"https://image.tmdb.org/t/p/w500{movie['poster_path']}"
         movieName = movie["title"]
         movieDescription = movie["overview"]
         movieReleaseDate = movie["release_date"]
         movieRating = movie["vote_average"]
+        moviePrice = "R$ 10,00"
 
         self.geometry("1000x1000")
         self.title(movieName)
 
         response = requests.get(posterUrl)
         img = Image.open(BytesIO(response.content))
-        ctk_image = ctk.CTkImage(img, size=(450, 450))
+        ctk_image = ctk.CTkImage(img, size=(300, 300))
 
-        movieFrame = ctk.CTkFrame(self)
+        movieFrame = ctk.CTkFrame(self, height=800, width=800)
 
-        movieFrame.pack()
 
         movieNameLabel = ctk.CTkLabel(movieFrame, text=movieName, font=("Papyrus", 40, "bold"))
         movieNameLabel.pack(padx=45)
@@ -35,16 +37,22 @@ class MovieWindow(ctk.CTkToplevel):
         movieImage = ctk.CTkLabel(movieFrame, image=ctk_image, text="")
         movieImage.pack()
 
-        movieDescriptionLabel = ctk.CTkTextbox(movieFrame, text=f"Descrição: {movieDescription}", font=("Papyrus", 20))
-        movieDescriptionLabel.pack(pady=10)
+        movieDescriptionLabel = ctk.CTkLabel(movieFrame, text=f"Descrição: {movieDescription}", font=("Papyrus", 20), wraplength=400)
+        movieDescriptionLabel.pack()
 
         movieReleaseDateLabel = ctk.CTkLabel(movieFrame, text=f"Data de lançamento: {movieReleaseDate}")
         movieReleaseDateLabel.pack(pady=10)
 
         movieRatingLabel = ctk.CTkLabel(movieFrame, text=f"Nota: {movieRating}")
         movieRatingLabel.pack(pady=10)
+
+        moviePriceLabel = ctk.CTkLabel(movieFrame, text=f"Preço: {moviePrice}")
+
+        movieFrame.pack()
+        moviePriceLabel.pack(pady=10)
         
-        rentButton = ctk.CTkButton(self, text="ALUGAR", command=self.addToCart)
+        rentButton = ctk.CTkButton(self, text="Alugar", command=self.addToCart)
+        
         rentButton.pack()
 
     def addToCart(self):
